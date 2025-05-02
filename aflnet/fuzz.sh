5ec0c4dc0c7f9827d408cbb910b8ee44ae4c480e
pd-fkie <77979557+pd-fkie@users.noreply.github.com>
2025-05-02 18:26:08 +0200

    Correct dict path

--- aflnet/fuzz.sh
+++ aflnet/fuzz.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 test -d /shared
-taskset -c 0 /aflnet/afl-fuzz -i /corpus/ -o /shared/output -m none -x smtp.dict -N tcp://127.0.0.1/2525 -P SMTP -K -E -R -q 3 -s 3 -- /exim-fuzzer/Exim/src/build-Linux-x86_64/exim -bdf
+taskset -c 0 /aflnet/afl-fuzz -i /corpus/ -o /shared/output -m none -x /exim-fuzzer/smtp.dict -N tcp://127.0.0.1/2525 -P SMTP -K -E -R -q 3 -s 3 -- /exim-fuzzer/Exim/src/build-Linux-x86_64/exim -bdf
 
